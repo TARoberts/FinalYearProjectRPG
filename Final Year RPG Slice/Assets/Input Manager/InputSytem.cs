d69a303838bc11641663d9a30e3b0023b717e4ce
Tom <tom3.roberts@live.uwe.ac.uk>
2022-07-04 17:03:46 +0100

    Inputs & Movement & Camera

---
+++ Final Year RPG Slice/Assets/Input Manager/InputSytem.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputSytem : MonoBehaviour
+{
+    public float jumpHeight, moveSpeed;
+    public bool inEditor;
+
+    private bool onFloor, attackLocked;
+
+    [SerializeField] private Transform checkSource, playerCameraPosition;
+    public LayerMask groundMask;
+    public float floorDistance;
+    private float gravityValue = -9.81f;
+    private Vector3 playerVelocity;
+
+    private DogKnightControls dogKnightControls;
+    private CharacterController dogController;
+
+    private void Awake()
+    {
+        
+        dogController = GetComponent<CharacterController>();
+
+        dogKnightControls = new DogKnightControls();
+        dogKnightControls.Player.Jump.Enable();
+        dogKnightControls.Player.Jump.performed += Jump;
+
+        dogKnightControls.Player.Attack.Enable();
+        dogKnightControls.Player.Attack.performed += Attack;
+
+        dogKnightControls.Player.Movement.Enable();
+        
+
+        if (!inEditor)
+        {
+            jumpHeight = jumpHeight * 2;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float localSpeed = moveSpeed;
+        onFloor = Physics.CheckSphere(checkSource.position, floorDistance, groundMask);
+        if (!onFloor)
+        {
+            localSpeed = localSpeed / 2;
+        }
+        
+        if (onFloor && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0;
+        }
+
+        Vector2 inputVector = dogKnightControls.Player.Movement.ReadValue<Vector2>();
+
+        Vector3 move = (new Vector3(inputVector.x, 0, inputVector.y));
+
+        move.Normalize();
+
+        move = Quaternion.AngleAxis(playerCameraPosition.rotation.eulerAngles.y, Vector3.up) * move;
+
+        // dogController.transform.Rotate(new Vector3(Time.deltaTime * 15f, 0f, 0f), Space.World);
+
+        dogController.Move(move * Time.deltaTime * localSpeed);
+
+        Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+        dogController.transform.rotation = Quaternion.RotateTowards(dogController.transform.rotation, toRotation, 0.1f * Time.deltaTime);
+        
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        dogController.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        onFloor = Physics.CheckSphere(checkSource.position, floorDistance, groundMask);
+        Debug.Log(onFloor);
+        if (onFloor)
+            {
+            Debug.Log("Jumpingway" + context.phase);
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -1.0f * gravityValue);
+            }
+        else
+            {
+                Debug.Log("Not grounded");
+            }
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+            attackLocked = true;
+            if (!attackLocked)
+            {
+                Debug.Log("Attackingway");
+                StartCoroutine(AttackLock());
+            }
+    }
+
+    IEnumerator AttackLock()
+    {
+        yield return new WaitForSeconds(1.0f);
+        attackLocked = false;
+    }
+}
+
