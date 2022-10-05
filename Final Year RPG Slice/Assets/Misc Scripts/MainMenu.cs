8a3a3e3199a68a3bbefea93ba127465e5470f563
Tom <tom3.roberts@live.uwe.ac.uk>
2022-10-05 15:12:46 +0100

    Menu Commit From June

---
+++ Final Year RPG Slice/Assets/Misc Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel;
+
+    public void ContinueGame()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exiting");
+    }
+}
